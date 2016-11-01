12c67c8e88cdef24093d7ec7ea8da402c41c296b
Sean Fernandez <scfernz@gmail.com>
2016-10-31 20:22:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+var ReactDOM = require('react-dom');
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello, world!</h1>
+    );
+  }
+}
+
+export default App;
